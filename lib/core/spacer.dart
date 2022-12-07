1b84d4f56749502e1e35db5797c9b46bdab94556
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 18:16:51 +0200

    new spacers

--- lib/core/spacer.dart
+++ lib/core/spacer.dart
@@ -3,8 +3,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget SpacerH5() => SizedBox(height: 5.h);
 Widget SpacerH10() => SizedBox(height: 10.h);
+Widget SpacerH13() => SizedBox(height: 13.6.h);
 Widget SpacerH15() => SizedBox(height: 15.h);
+Widget SpacerH17() => SizedBox(height: 17.5.h);
+Widget SpacerH18() => SizedBox(height: 18.h);
+Widget SpacerH20() => SizedBox(height: 20.h);
 
 Widget SpacerW5() => SizedBox(width: 5.w);
 Widget SpacerW10() => SizedBox(width: 10.w);
 Widget SpacerW15() => SizedBox(width: 15.w);
+Widget SpacerW29() => SizedBox(width: 29.w);
