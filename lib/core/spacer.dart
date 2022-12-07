de1f6167effd466ab654558d40fb4b6dfa1dece3
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 18:25:27 +0200

    add new spaces

--- lib/core/spacer.dart
+++ lib/core/spacer.dart
@@ -3,11 +3,14 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget SpacerH5() => SizedBox(height: 5.h);
 Widget SpacerH10() => SizedBox(height: 10.h);
+Widget SpacerH12() => SizedBox(height: 12.h);
 Widget SpacerH13() => SizedBox(height: 13.6.h);
 Widget SpacerH15() => SizedBox(height: 15.h);
 Widget SpacerH17() => SizedBox(height: 17.5.h);
 Widget SpacerH18() => SizedBox(height: 18.h);
+Widget SpacerH19() => SizedBox(height: 19.h);
 Widget SpacerH20() => SizedBox(height: 20.h);
+Widget SpacerH21() => SizedBox(height: 21.1.h);
 
 Widget SpacerW5() => SizedBox(width: 5.w);
 Widget SpacerW10() => SizedBox(width: 10.w);
