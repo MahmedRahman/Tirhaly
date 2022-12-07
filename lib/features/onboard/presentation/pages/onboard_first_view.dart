86d988821f3a155ba6f7ea162a03505d8181a00f
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 16:47:19 +0200

    use new custom widget

--- lib/features/onboard/presentation/pages/onboard_first_view.dart
+++ lib/features/onboard/presentation/pages/onboard_first_view.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:thrhaly/core/image_path.dart';
 import 'package:thrhaly/features/onboard/presentation/controller/onboard_first_screen_controller.dart';
+import 'package:thrhaly/features/onboard/presentation/widgets/base_on_boarding_page.dart';
 
 class OnboardFirstView extends GetView<OnboardScreenController> {
   final OnboardScreenController controller = Get.put(OnboardScreenController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Image.asset(
-        "assets/images/Onboard_Screen_1.png",
-        fit: BoxFit.cover,
-      ),
+    return const BaseOnBoardingPage(
+      imagePath: AppImagePath.onBoardingOne,
     );
   }
 }
-
-
-
--- lib/features/onboard/presentation/pages/onborad_second_view.dart
+++ lib/features/onboard/presentation/pages/onborad_second_view.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:thrhaly/core/image_path.dart';
 import 'package:thrhaly/features/onboard/presentation/controller/onboard_second_screen_controller.dart';
+import 'package:thrhaly/features/onboard/presentation/widgets/base_on_boarding_page.dart';
 
 class OnboardSecondView extends GetView<OnboardSecondScreenController> {
-  final OnboardSecondScreenController controller = Get.put(OnboardSecondScreenController());
+  final OnboardSecondScreenController controller =
+      Get.put(OnboardSecondScreenController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Image.asset(
-        "assets/images/Onboard_Screen_2.png",
-        fit: BoxFit.cover,
-      ),
+    return const BaseOnBoardingPage(
+      imagePath: AppImagePath.onBoardingTwo,
     );
   }
 }
