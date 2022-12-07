413e83a46c73f0e6d0c9ca199db0df220e5362ad
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 16:45:34 +0200

    complete login view

--- lib/features/auth/presentation/pages/login_view.dart
+++ lib/features/auth/presentation/pages/login_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:thrhaly/core/image_path.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/skip_button.dart';
 import 'package:thrhaly/features/home/presentation/pages/home_view.dart';
+import 'package:thrhaly/features/onboard/presentation/widgets/base_on_boarding_page.dart';
 import '../widgets/login_button.dart';
 import 'package:thrhaly/core/const.dart';
 import 'package:thrhaly/features/auth/presentation/pages/sign_in_view.dart';
@@ -12,40 +13,41 @@ class LoginView extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            Image.asset(
-              AppImagePath.Onboard_Screen_3,
-              fit: BoxFit.fill,
-            ),
-            Positioned(
-              right: 0,
-              child: SkipButton(
-                onTap: () {
-                  Get.to(() => HomeView());
-                },
-              ),
-            ),
-            Positioned(
-              bottom: 20.h,
-              left: 0,
-              right: 0,
-              child: LoginButton(
-                ButtonBackGroundColor: Color(
-                  AppConst.KColorGreen,
-                ),
-                title: "sign in".tr,
-                onPressed: () {
-                  Get.to(
-                    () => SignInView(),
-                  );
-                },
-              ),
-            )
-          ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        centerTitle: false,
+        title: SkipButton(
+          onTap: () {
+            Get.to(() => HomeView());
+          },
         ),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          const BaseOnBoardingPage(
+            imagePath: AppImagePath.Onboard_Screen_3,
+          ),
+          Positioned(
+            bottom: 43.h,
+            left: 20.w,
+            right: 20.w,
+            child: LoginButton(
+              ButtonBackGroundColor: Color(
+                AppConst.KColorGreen,
+              ),
+              title: "sign in".tr,
+              onPressed: () {
+                Get.to(
+                  () => SignInView(),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
