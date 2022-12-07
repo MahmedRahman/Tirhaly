e70c43656a162e9631d35856f5349f3203ab3e9f
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 18:28:33 +0200

    add app bar

--- lib/features/auth/presentation/widgets/base_auth_page.dart
+++ lib/features/auth/presentation/widgets/base_auth_page.dart
@@ -3,12 +3,15 @@ import 'package:get/get.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/my_logo.dart';
 
 class BaseAuthPage extends GetView {
-  const BaseAuthPage({Key? key, required this.child}) : super(key: key);
+  const BaseAuthPage({Key? key, required this.child, this.appBar})
+      : super(key: key);
 
   final Widget child;
+  final PreferredSizeWidget? appBar;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appBar,
       body: SafeArea(
         child: Column(
           children: [
