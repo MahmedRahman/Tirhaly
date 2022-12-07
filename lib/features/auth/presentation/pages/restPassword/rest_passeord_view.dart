c7814b175fc8f0ec678a940ea74793418280ec8c
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 20:13:35 +0200

    complete rest password view

--- lib/features/auth/presentation/pages/restPassword/rest_passeord_view.dart
+++ lib/features/auth/presentation/pages/restPassword/rest_passeord_view.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:thrhaly/core/const.dart';
 import 'package:thrhaly/core/spacer.dart';
-import 'package:thrhaly/features/auth/presentation/pages/restPassword/otp_view.dart';
-import 'package:thrhaly/features/auth/presentation/pages/sigin_up_view.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/back_button.dart';
+import 'package:thrhaly/features/auth/presentation/widgets/base_auth_page.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/form_title.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/login_button.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_check_box.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_social_button.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_text_button.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/my_text_form_field.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_two_text_button.dart';
 import 'package:thrhaly/features/home/presentation/pages/home_view.dart';
 
 class ResetPasswordView extends GetView {
@@ -21,58 +16,59 @@ class ResetPasswordView extends GetView {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
+    return BaseAuthPage(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0,
+          actions: [
+            backButton(
+              onPressed: () {
+                Get.back();
+              },
+            )
+          ],
+        ),
         child: Form(
           autovalidateMode: AutovalidateMode.onUserInteraction,
           key: _formKey,
-          child: ListView(
+          child: Column(
             children: [
-              SpacerH10(),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    backButton(onPressed: () {
-                      Get.back();
-                    }),
-                    FormTitle(
-                      firstText: "إعادة",
-                      scandText: "كلمة المرور",
-                    ),
-                    SpacerH15(),
-                    MyTextFormField(
-                      tag: "pass",
-                      labelText: "كلمة المرور",
-                      hintText: "ادخل كلمة المرور",
-                      AssetImageIcon: "assets/images/icon_pass.png",
-                      obscureText: true,
-                      validator: (value) {
-                        if (value!.isEmpty == true || value == "") {
-                          return "حقل مطلوب";
-                        }
-                        return null;
-                      },
-                    ),
-                    SpacerH10(),
-                    MyTextFormField(
-                      tag: "pass",
-                      labelText: "إعادة كلمة المرور",
-                      hintText: "ادخل كلمة المرور",
-                      AssetImageIcon: "assets/images/icon_pass.png",
-                      obscureText: true,
-                      validator: (value) {
-                        if (value!.isEmpty == true || value == "") {
-                          return "حقل مطلوب";
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
+              SizedBox(
+                height: 73.h,
               ),
+              FormTitle(
+                firstText: "إعادة",
+                scandText: "كلمة المرور",
+              ),
+              SpacerH19(),
+              MyTextFormField(
+                tag: "pass",
+                labelText: "كلمة المرور",
+                hintText: "ادخل كلمة المرور",
+                AssetImageIcon: "assets/images/icon_pass.png",
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty == true || value == "") {
+                    return "حقل مطلوب";
+                  }
+                  return null;
+                },
+              ),
+              SpacerH15(),
+              MyTextFormField(
+                tag: "pass",
+                labelText: "إعادة كلمة المرور",
+                hintText: "ادخل كلمة المرور",
+                AssetImageIcon: "assets/images/icon_pass.png",
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty == true || value == "") {
+                    return "حقل مطلوب";
+                  }
+                  return null;
+                },
+              ),
+              SpacerH18(),
               LoginButton(
                 ButtonBackGroundColor: Color(AppConst.KColorBlue),
                 title: "حفظ",
@@ -85,8 +81,6 @@ class ResetPasswordView extends GetView {
               ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
