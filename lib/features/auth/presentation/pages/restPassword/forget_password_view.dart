059a6dddfdbf64eaf9b58d2184febeb864cf3d26
abdullahehab <nrtroz.ae@gmail.com>
2022-12-07 18:21:20 +0200

    forget password page

--- lib/features/auth/presentation/pages/restPassword/forget_password_view.dart
+++ lib/features/auth/presentation/pages/restPassword/forget_password_view.dart
@@ -1,84 +1,79 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:thrhaly/core/const.dart';
 import 'package:thrhaly/core/spacer.dart';
 import 'package:thrhaly/features/auth/presentation/pages/restPassword/otp_view.dart';
-import 'package:thrhaly/features/auth/presentation/pages/sigin_up_view.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/back_button.dart';
+import 'package:thrhaly/features/auth/presentation/widgets/base_auth_page.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/form_title.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/login_button.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_check_box.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_social_button.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_text_button.dart';
 import 'package:thrhaly/features/auth/presentation/widgets/my_text_form_field.dart';
-import 'package:thrhaly/features/auth/presentation/widgets/my_two_text_button.dart';
 
 class ForgetPasswordView extends GetView {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Form(
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          key: _formKey,
-          child: ListView(
-            children: [
-              SpacerH10(),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    backButton(onPressed: () {
-                      Get.back();
-                    }),
-                        FormTitle(
-                      firstText: "نسيت",
-                      scandText: "كلمة المرور؟",
-                    ),
-                    SpacerH15(),
-                    MyTextFormField(
-                      tag: "email",
-                      labelText: "البريد الإلكتروني",
-                      hintText: "ادخل البريد الإلكتروني",
-                      AssetImageIcon: "assets/images/icon_email.png",
-                      validator: (value) {
-                        if (value!.isEmpty == true || value == "") {
-                          return "حقل مطلوب";
-                        }
-                        return null;
-                      },
-                    ),
-                    SpacerH10(),
-                    Text(
-                      "الرجاء كتابة البريد الإلكتروني الخاص بك أدناه \n للحصول على كود التأكيد",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w300,
-                        color: Color(AppConst.KColorBlue),
-                      ),
-                    ),
-                  ],
-                ),
+    return BaseAuthPage(
+      child: Form(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 73.h,
+            ),
+            FormTitle(
+              firstText: "نسيت",
+              scandText: "كلمة المرور؟",
+            ),
+            SpacerH10(),
+            Text(
+              "الرجاء كتابة البريد الإلكتروني الخاص بك أدناه \n للحصول على كود التأكيد",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+                color: Color(AppConst.KColorBlue),
               ),
-              LoginButton(
-                ButtonBackGroundColor: Color(AppConst.KColorBlue),
-                title: "إرسال",
-                onPressed: () {
-                  _formKey.currentState!.validate();
-                  Get.to(() => OtpView());
-                },
+            ),
+            SpacerH15(),
+            FadeInUp(
+              from: 30,
+              duration: const Duration(milliseconds: 500),
+              child: Column(
+                children: [
+                  MyTextFormField(
+                    tag: "email",
+                    labelText: "البريد الإلكتروني",
+                    hintText: "ادخل البريد الإلكتروني",
+                    AssetImageIcon: "assets/images/icon_email.png",
+                    validator: (value) {
+                      if (value!.isEmpty == true || value == "") {
+                        return "حقل مطلوب";
+                      }
+                      return null;
+                    },
+                  ),
+                  SpacerH15(),
+                  LoginButton(
+                    ButtonBackGroundColor: Color(AppConst.KColorBlue),
+                    title: "إرسال",
+                    onPressed: () {
+                      _formKey.currentState!.validate();
+                      Get.to(() => OtpView());
+                    },
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 15,
-              ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+          ],
         ),
       ),
     );
